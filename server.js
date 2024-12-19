af217bc6c20414183f505ba6a0d69c0d069b7ea6
bschloss <benschloss2@gmail.com>
2024-12-19 18:00:50 -0500

    Adding cors.

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ import testJWTRouter from './controllers/test-jwt.js';
 import usersRouter  from './controllers/users.js';
 import profilesRouter from './controllers/profiles.js';
 import recommendationsRouter from './controllers/recommendations.js';
+import cors from 'cors'
 
 mongoose.connect(process.env.MONGODB_URI);
 
@@ -18,6 +19,7 @@ mongoose.connection.on('connected', () => {
 
 
 app.use(express.json());
+app.use(cors())
 app.use('/test-jwt', testJWTRouter);
 app.use('/users', usersRouter);
 app.use('/profiles', profilesRouter);
